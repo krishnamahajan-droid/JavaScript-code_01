afb5578ccfca19bfa05835723d7b0b1d056f07fa
krishna-mahajan-droid <krishnamahajanb9@gmail.com>
2026-04-27 10:46:24 +0530

    Updated Code

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -3,22 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { add, subtract } from './utility'
+import {add,subtract} from './utility'
 import Sample from './component/Sample'
 import HomePage from './component/HomePage'
 import Contact from './component/Contact'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import Profile from './component/Profile'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const name="Profile Page Name"
+
 
   return (
-    <BrowserRouter>
+    <>
+
       <Routes>
-        <Route path="/" element={<><HomePage/><Sample count={count} setCount={setCount}/></>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path='/' element={<><HomePage/><Sample/></>}/>
+        <Route path='homepage' element={<HomePage name={name}/>}>
+          <Route path='sample' element={<Sample/>}/>
+        </Route>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/profile/:username' element={<Profile/>}/>
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
--- reactapp/src/component/Homepage.jsx
+++ reactapp/src/component/Homepage.jsx
@@ -1,7 +1,10 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import Profile from './Profile'
 
 function HomePage() {
+function HomePage({name}) {
     const [login, setLogin] = useState(false)
 //   if(login){
 //     return<><button onClick={()=>setLogin(false)}>Logout</button></>
@@ -14,10 +17,14 @@ function HomePage() {
     {login?<button onClick={()=>setLogin(false)}>Logout</button>:<button onClick={()=>setLogin(true)}>Login</button>}
     <ul>
       <li><Link to="/">HomePage</Link></li>
+      <li><Link to="/homepage">HomePage</Link></li>
+      <li><NavLink to="/homepage" style={({isActive})=>({color:isActive?"red":"blue"})}>HomePage</NavLink></li>
       <li><Link to="/contact">Contact</Link></li>
+      <li><NavLink to="/contact" style={({isActive})=>({color:isActive?"red":"blue"})}>Contact</NavLink></li>
     </ul>
+      <Profile name={name} />
     </>
   )
-}
+}}
 
 export default HomePage
